815dbaff23193c8485b6ad40e54bedc3eddbfc94
Anatolii <37179196+AnatoliiKyrylenko@users.noreply.github.com>
2018-06-16 16:03:38 +0300

    Create BackupWS.sh

---
+++ BackupWS.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Backup Web Server
+# Tested Ubuntu+LAMP
+
+# Variable "today"
+nowadays=$(date +"%Y-%m-%d")
+
+# Creating work directory
+mkdir /home/backup/
+
+# Cleanup old backups
+rm -rf /home/backup/*
+
+# Archiving sites & server
+tar cfvzp /home/backup/www.tgz /var/www/
+tar cfvzp /home/backup/etc.tgz /etc/
+
+# Archiving all databases
+mysqldump -u root -p --all_databases | gzip > /home/backup/mysql.sql.gz
+
+# Archiving sites & server & databases
+tar cfvP /home/backup/backup_$nowadays.tar /home/backup/*
+
+# Cleanup work directory
+rm -rf /home/backup/*.tgz
+rm -rf /home/backup/*.gz
+
+echo "Backup file created, see /home/backup/"
